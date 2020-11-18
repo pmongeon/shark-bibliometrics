0d8720468bf108cedd59260d952670539f582ebe
Philippe <philippe.mongeon@gmail.com>
2020-11-18 11:24:56 -0400

    update

---
+++ Boyack_Klavans.R
@@ -0,0 +1,327 @@
+#***********************
+# Project: Mapping LIS
+# Description: This code generates the network files for Gephi
+# Author: Philippe Mongeon
+# Date: 2020-07-31
+#***********************
+
+# Load needed packages ----
+library(sqldf) # for running SQL in R
+library(rio) # for the import function
+library(stringr)
+library(tidyverse)
+library(data.table) # For the parsing
+library(tm) # For the stemming
+library(readxl)
+library(tidytext)
+library(textstem)
+
+# Load data ----
+data <- import("data/sigmet.txt", quote="") %>%  # Loader les données
+  select(PersonID, name = "Full name", id_art, Title, Abstract, Affiliation, Year) %>%  # Garder seulement les colonnes necessaires
+  filter(Abstract != "") %>% 
+  unique()
+
+citations <- import("data/sigmet_citations.txt") %>%
+  filter(ID_Art_Citant != "NULL") %>% 
+  mutate(ID_Art_Citant = as.integer(ID_Art_Citant)) %>% 
+  mutate(ID_Art_Cite = as.integer(ID_Art_Cite))
+  
+
+# Faire une liste avec  deux colonnes : 
+## 1) id_art 
+## 2) article cité OU mot du tire
+
+citations <- data %>% 
+  inner_join(citations, by = c("id_art" = "ID_Art_Citant")) %>% 
+  select(id_art, ID_Art_Cite)
+
+
+# parser les titres + abstracts pour faire la table id_art | mot
+text <- data %>% 
+  unite(text, c("Title", "Abstract")) %>% # Concatene le titre et l'abstract dans la colonne text
+  select(id_art, text) %>%  # selectionne seulement les colonnes id_art et text
+  unique()
+
+
+# nettoyer le texte
+#
+#clean <- function(x) {
+#  # make text lower case
+#  x <- str_to_lower(x)
+#  # remove non-alphanumeric symbols
+#  x <- str_replace_all(x, "[^[:alnum:]]", " ")
+#  # collapse multiple spaces
+#  str_replace_all(x, "\\s+", " ")
+#  # remove leading and trailing whitespace
+#  trimws(x)
+#}
+
+#text_clean <- text %>% 
+# mutate(text = clean(text))
+
+text <- text %>% 
+  unnest_tokens(word, text, token = "ngrams", n = 1) %>% 
+  select(id_art, word) %>% 
+  drop_na(word) %>% 
+  mutate(stemmed = stem_words(word))
+
+# Remove stop words
+
+sw <- read.delim("data/NLTK_stopwords.txt", header = FALSE) %>% 
+  rename(word = V1) %>%
+  mutate(word = stem_words(word))
+
+text <- text %>%
+  anti_join(sw, by = ("word"))
+
+
+# TF IDF ----
+
+text <- text %>% 
+  group_by(id_art, word) %>% 
+  summarise(freq = n()) %>% 
+  ungroup()
+
+# TF 
+## (TF(t) = (Number of times term t appears in a document) / (Total number of terms in the document).
+
+text <- text %>%
+  group_by(id_art) %>% 
+  mutate(total_words = sum(freq)) %>% 
+  ungroup() %>% 
+  mutate(tf = freq/total_words) %>% 
+  select(id_art, total_words, word, freq, tf)
+
+# IDF
+## IDF(t) = log_e(Total number of documents / Number of documents with term t in it)
+
+### count number of documents
+n_doc <- length(unique(textid_art))
+
+### count number of documents for each word
+idf <- text %>%
+  group_by(word) %>% 
+  summarise(total_words = n()) %>% 
+  ungroup() %>% 
+  mutate(total = n_doc) %>% 
+  mutate(idf = log(total/total_words)) %>% 
+  select(word, idf)
+
+# TF-IDF
+
+text <- text %>% 
+  inner_join(idf, by = "word") %>% 
+  mutate(tf_idf = tf*idf) %>% 
+  select(id_art, word, freq, tf_idf) %>% 
+  group_by(id_art) %>% 
+  mutate(sum_tf_idf = sum("^"(tf_idf,2))) %>% 
+  ungroup()
+
+table <- rbind(rename(mutate(citations, tf_idf=1), "word" = "ID_Art_Cite"), select(text, id_art, word, tf_idf))
+
+
+# bibliographic coupling ----
+BCNetwork <- sqldf("
+      SELECT DISTINCT
+        a.id_art as Source,
+        b.id_art as Target,
+        sum((a.tf_idf+b.tf_idf)/2) as Weight,
+        'undirected' as Type
+      FROM [table] a
+      JOIN [table] b on b.word = a.word and b.id_art != a.id_art
+      GROUP BY a.id_art, b.id_art
+      ORDER BY sum((a.tf_idf+b.tf_idf)/2)
+      ")
+
+BCPeopleNetwork <- sqldf("
+      SELECT DISTINCT
+        b.name as Source,
+        c.name as Target,
+        sum(Weight) as Weight,
+        'undirected' as Type
+      FROM BCNetwork a
+      JOIN data b on b.id_art = a.source
+      JOIN data c on c.id_art = a.target  and c.name > b.name
+      GROUP BY b.name, c.name
+      ORDER BY sum(Weight)
+      ")
+
+
+remove(text, idf, n_doc, table)
+
+# Export network files ----
+# Because Gephi works best with CSV files, I export the data frame in CSV
+# by default write.csv will include the row name (by default the row numbers), so I include row.names = FALSE because I don't want those).
+
+write.csv(BCNetwork,"Networks/hybrid_network_sigmet.csv", row.names = FALSE)
+
+# Nodes ----
+
+# affiliations
+
+
+nodes <- sqldf("select distinct id_art, Affiliation from data where Affiliation != ''")
+
+nodes <- data.table(nodes)
+
+nodes <- nodes[,c(Affiliation=strsplit(Affiliation, " ", fixed = TRUE)), by = id_art] %>% 
+  filter(Affiliation != "")
+
+
+
+
+
+nodes <- sqldf("
+               select a.id_art as id, a.Title as label, a.Year, 
+               case when b.id_art is not null then 1 else 0 end as McGill,
+               case when c.id_art is not null then 1 else 0 end as Montreal,
+               case when d.id_art is not null then 1 else 0 end as Toronto,
+               case when e.id_art is not null then 1 else 0 end as UBC,
+               case when f.id_art is not null then 1 else 0 end as Alberta,
+               case when g.id_art is not null then 1 else 0 end as Ottawa,
+               case when h.id_art is not null then 1 else 0 end as Western,
+               case when i.id_art is not null then 1 else 0 end as Dalhousie
+               from data a
+               left join nodes b on b.id_art = a.id_art and b.Affiliation = 'MCGILL-UNIV'
+               left join nodes c on c.id_art = a.id_art and c.Affiliation = 'UNIV-MONTREAL'
+               left join nodes d on d.id_art = a.id_art and d.Affiliation = 'UNIV-TORONTO'
+               left join nodes e on e.id_art = a.id_art and e.Affiliation = 'UNIV-BRITISH-COLUMBIA'
+               left join nodes f on f.id_art = a.id_art and f.Affiliation = 'UNIV-ALBERTA'
+               left join nodes g on g.id_art = a.id_art and g.Affiliation = 'UNIV-OTTAWA'
+               left join nodes h on h.id_art = a.id_art and h.Affiliation = 'UNIV-WESTERN-ONTARIO'
+               left join nodes i on i.id_art = a.id_art and i.Affiliation = 'DALHOUSIE-UNIV'
+               ")
+
+write.csv(nodes,"Networks/hybrid_network_sigmet_nodes.csv", row.names = FALSE) 
+
+remove(nodes)
+# Clustering ----
+
+# Libraries --------------------------------------------------------------
+
+
+library(igraph)
+
+
+# Data Preparation -------------------------------------------------------
+
+#Load dataset
+edges <- BCNetwork %>% 
+  select(Source,Target,Weight) %>% 
+  filter(Weight >= 0.5)
+  
+colnames(edges) <- c("from", "to", "weight")
+
+#Create graph for the algorithms
+g <- graph_from_data_frame(edges, directed = FALSE)
+
+remove(edges)
+
+# Community Detection ----------------------------------------------------
+
+# Louvain
+lc <- cluster_louvain(g)
+membership(lc)
+communities(lc)
+
+clusters <- communities(lc)
+
+clustering <- as.data.frame(clusters[[1]]) %>% 
+  mutate(cluster = 1) %>% 
+  rename("id_art" = "clusters[[1]]")
+
+for (i in 2:length(clusters)){
+  x <- as.data.frame(clusters[[i]]) %>% 
+          mutate(cluster = i)
+  colnames(x) <- c("id_art", "cluster")
+  clustering <- rbind(clustering,x)
+}
+
+remove(x)
+
+clustering <- clustering %>% 
+  mutate(id_art = as.integer(as.character(id_art)))
+
+# Infomap
+# imc <- cluster_infomap(g)
+# membership(imc)
+# communities(imc)
+# plot(lc, g)
+
+# Visualizing ----
+
+gephi_cluster <- read.csv("data/gephi_export.csv") %>% 
+  rename("id_art"="Id", "cluster"="modularity_class")
+
+gephi_cluster[gephi_cluster$cluster == "28"] <- "20"
+
+x <- sqldf("select distinct cluster, count(distinct id_art) as n from gephi_cluster group by cluster order by count(distinct id_art) desc") %>% 
+  filter(n >= 10)
+
+
+data_final <- data %>% 
+  left_join(nodes, by = "id_art") %>% 
+  inner_join(gephi_cluster, by="id_art") %>%
+  inner_join(x, by="cluster") %>% 
+  select(PersonID, id_art, Title, Affiliation = Affiliation.x, Year, cluster) %>% 
+  unique()
+
+data_person <- data_final %>% 
+  select(PersonID, id_art) %>% 
+  unique() %>% 
+  group_by(PersonID) %>% 
+  mutate(person_total = n()) %>%
+  ungroup() %>% 
+  mutate(pct_total = 1/person_total) %>% 
+  left_join(gephi_cluster, by="id_art") %>% 
+  group_by(PersonID,cluster) %>% 
+  mutate(cluster_total = n()) %>% 
+  mutate(pct_cluster = sum(pct_total)) %>% 
+  ungroup() %>% 
+  select(PersonID, cluster, pct_cluster) %>% 
+  unique()
+
+sqldf("select * from data_person where PersonID = 227")
+
+data_person <- data_final %>% 
+  select(PersonID, Affiliation, Year, cluster) %>% 
+  unique() %>% 
+  left_join(data_person, by=c("PersonID","cluster"))
+  
+data_univ <- data_person %>% 
+  select(Affiliation, cluster, pct_cluster) %>% 
+  unique() %>% 
+  group_by(Affiliation, cluster) %>% 
+  mutate(total = sum(pct_cluster)) %>% 
+  ungroup %>% 
+  select(Affiliation, cluster, total) %>% 
+  unique()
+
+
+write_delim(data_person,"data/data_person.txt", delim="\t")
+
+write_delim(data_final,"data/data_sigmet.txt", delim="\t") 
+write_delim(data_univ,"data/data_univ_sigmet.txt", delim="\t")
+
+
+
+x <- sqldf("SELECT DISTINCT b.cluster, a.word, count(distinct a.id_art) as n
+           from text_stemmed a
+           join gephi_cluster b on b.id_art = a.id_art
+           where tf_idf >= 0.1
+           group by b.cluster, a.word
+           ")
+x <- sqldf("Select distinct a.*, row_number() over (partition by a.cluster order by a.n desc) as rk
+            from x a 
+           ")
+
+y <- 20
+filter(x, cluster == y, rk <= 20)
+  
+
+
+
+
+
+
--- Networks.R
+++ Networks.R
@@ -7,81 +7,85 @@
 
 # Load needed packages ----
 library(sqldf)
-library(rio) # for the import function
+library(tidyverse)
+library(readxl)
+
+# Working directory
+setwd("C:/Users/Philippe/Documents/GitHub/shark-project")
 
 # Load data ----
-Articles <- import("data/lis_articles.txt")
-Keywords <- import("data/lis_keyword.txt")
-Citations <- import("data/lis_citations.txt")
-Abstract <- import("data/lis_abstracts.txt")
+Articles <- read_excel("shark_data.xlsx", sheet ="papers")
+Citations <- read_excel("shark_data.xlsx", sheet ="citations")
 
-# keyword co-occurence ----
 
-CONetwork <- sqldf("
-      SELECT DISTINCT
-        a.keyword as tource,
-        b.keyword as target,
-        count(distinct a.id_art) as weight,
-        'undirected' as type
-      FROM Keywords a
-      JOIN keywords b on b.id_art = a.id_art and b.keyword > a.keyword 
-      WHERE a.keyword != 'NULL' and b.keyword != 'NULL'
-      GROUP BY a.keyword, b.keyword
-      ORDER BY count(distinct a.id_art) desc
-      ")
+
+# keyword co-occurence ---- NOT CURRENTLY OPERATIONAL
+
+# CONetwork <- sqldf("
+#      SELECT DISTINCT
+#        a.keyword as Source,
+#        b.keyword as Target,
+#        count(distinct a.ut) as Weight,
+#        'undirected' as Type
+#      FROM Keywords a
+#      JOIN keywords b on b.ut = a.ut and b.keyword > a.keyword 
+#      WHERE a.keyword != 'NULL' and b.keyword != 'NULL'
+#      GROUP BY a.keyword, b.keyword
+#      ORDER BY count(distinct a.ut) desc
+#      ")
 
 # bibliographic coupling ----
 BCNetwork <- sqldf("
       SELECT DISTINCT
-        a.id_art as Source,
-        d.id_art as Target,
-        count(distinct b.id_art_cite) as Weight,
+        a.ut as Source,
+        d.ut as Target,
+        count(distinct b.cited_ut) as Weight,
         'undirected' as Type
       FROM Articles a
-      JOIN Citations b on b.id_art_citant = a.id_art
-      JOIN Citations c on c.id_art_cite = b.id_art_cite
-      JOIN Articles d on d.id_art = c.id_art_citant
-      WHERE d.id_art > a.id_art
-      GROUP BY a.id_art, d.id_art
-      ORDER BY count(distinct b.id_art_cite) desc
+      JOIN Citations b on b.citing_ut = a.ut
+      JOIN Citations c on c.cited_ut = b.cited_ut
+      JOIN Articles d on d.ut = c.citing_ut
+      WHERE d.ut > a.ut
+      GROUP BY a.ut, d.ut
+      ORDER BY count(distinct b.cited_ut) desc
       ")
 
 # Co-citation ----
 CCNetwork <- sqldf("
       SELECT DISTINCT
-        a.id_art as Source,
-        d.id_art as Target,
-        count(distinct b.id_art_citant) as Weight,
+        a.ut as Source,
+        d.ut as Target,
+        count(distinct b.citing_ut) as Weight,
         'undirected' as Type
       FROM Articles a
-      JOIN Citations b on b.id_art_cite = a.id_art
-      JOIN Citations c on c.id_art_citant = b.id_art_citant
-      JOIN Articles d on d.id_art = c.id_art_cite
-      WHERE d.id_art > a.id_art
-      GROUP BY a.id_art, d.id_art
-      ORDER BY count(distinct b.id_art_cite) desc
+      JOIN Citations b on b.cited_ut = a.ut
+      JOIN Citations c on c.citing_ut = b.citing_ut
+      JOIN Articles d on d.ut = c.cited_ut
+      WHERE d.ut > a.ut
+      GROUP BY a.ut, d.ut
+      ORDER BY count(distinct b.cited_ut) desc
       ")
 
 # Direct citation ----
 DCNetwork <- sqldf("
       SELECT DISTINCT
-        a.id_art as Source,
-        c.id_art as Target,
+        a.ut as Source,
+        c.ut as Target,
         1 as Weight,
         'directed' as Type
       FROM Articles a
-      JOIN Citations b on b.id_art_citant = a.id_art
-      JOIN Articles c on c.id_art = b.id_art_cite
+      JOIN Citations b on b.citing_ut = a.ut
+      JOIN Articles c on c.ut = b.cited_ut
       ")
 
 # Export network files ----
 # Because Gephi works best with CSV files, I export the data frame in CSV
 # by default write.csv will include the row name (by default the row numbers), so I include row.names = FALSE because I don't want those).
 
-write.csv(BCNetwork,"Networks/BC_network_article.csv", row.names = FALSE) 
-write.csv(DCNetwork,"Networks/DC_network_article.csv", row.names = FALSE) 
-write.csv(CCNetwork,"Networks/CC_netwrok_articles.csv", row.names = FALSE) 
-write.csv(CONetwork,"Networks/CO_network_keywords.csv", row.names = FALSE) 
+write.csv(BCNetwork,"networks/BC_network_article.csv", row.names = FALSE) 
+write.csv(DCNetwork,"networks/DC_network_article.csv", row.names = FALSE) 
+write.csv(CCNetwork,"networks/CC_network_articles.csv", row.names = FALSE) 
+# write.csv(CONetwork,"networks/CO_network_keywords.csv", row.names = FALSE) # CO_Network not currently operational
 
 # Clustering ----
 
